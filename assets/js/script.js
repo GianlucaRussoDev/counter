82ad90856f2289353852bfdc5551d1ac88fc32c3
GianlucaRussoDev <gianlucarussodev@gmail.com>
2024-02-21 14:47:51 +0100

    v2
    Cambiamenti per esame
    
    - creazione elementi dom
    - aggiunta favicon
    -aggiunti metadata da modificare

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,34 @@
-const auBtn = document.getElementById("au-btn");
-const deBtn = document.getElementById("de-btn");
-const counterView = document.getElementById("counter-view");
-let counter = 0; 
-function increment() {
-    counter++;
-    counterView.innerHTML = counter;
-}
-function decrement() {
-    counter--;
-    counterView.innerHTML = counter;   
-}
-auBtn.addEventListener("click", increment);
-deBtn.addEventListener("click", decrement);
+const container = document.querySelector('.container');
+const title = document.createElement('h1');
+title.id = 'title';
+title.textContent = 'Counter XP-10';
+container.appendChild(title);
+const counterDiv = document.createElement('div');
+counterDiv.classList.add('counter');
+const counterView = document.createElement('div');
+counterView.id = 'counter-view';
+counterView.textContent = '0';
+counterDiv.appendChild(counterView);
+const increaseButton = document.createElement('button');
+increaseButton.classList.add('btn');
+increaseButton.id = 'au-btn';
+increaseButton.textContent = '+';
+counterDiv.appendChild(increaseButton);
+const decreaseButton = document.createElement('button');
+decreaseButton.classList.add('btn');
+decreaseButton.id = 'de-btn';
+decreaseButton.textContent = '-';
+counterDiv.appendChild(decreaseButton);
+container.appendChild(counterDiv);
+
+increaseButton.addEventListener('click', () => {
+    let currentValue = parseInt(counterView.textContent);
+    currentValue++;
+    counterView.textContent = currentValue;
+});
+
+decreaseButton.addEventListener('click', () => {
+    let currentValue = parseInt(counterView.textContent);
+    currentValue--;
+    counterView.textContent = currentValue;
+});
