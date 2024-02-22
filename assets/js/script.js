899fe0d5d2871bb4bcdefbe8133c269fbc8ba45c
GianlucaRussoDev <gianlucarussodev@gmail.com>
2024-02-22 14:11:47 +0100

    v5
    space in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
 const container = document.querySelector('.container');
+
 const title = document.createElement('h1');
 title.id = 'title';
 title.textContent = 'Counter XP-10';
 container.appendChild(title);
+
 const counterDiv = document.createElement('div');
 counterDiv.classList.add('counter');
+
 const counterView = document.createElement('div');
 counterView.id = 'counter-view';
 counterView.textContent = '0';
 counterDiv.appendChild(counterView);
+
 const increaseButton = document.createElement('button');
 increaseButton.classList.add('btn');
 increaseButton.id = 'au-btn';
 increaseButton.textContent = '+';
 counterDiv.appendChild(increaseButton);
+
 const decreaseButton = document.createElement('button');
 decreaseButton.classList.add('btn');
 decreaseButton.id = 'de-btn';
